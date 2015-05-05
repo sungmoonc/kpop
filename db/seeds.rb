4cd3303f6d34475aaa74088834271fbec0112654
Erin Hyejin Kim <lvhrhj@gmail.com>
2015-05-05 15:52:57 -0700

    schema design and load csv

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+require 'csv'
+
+Video.delete_all
+file_to_load = File.dirname(__FILE__) + "/kpoplist.csv"
+
+csv_text = File.read(file_to_load)
+csv = CSV.parse(csv_text, :headers => true)
+csv.each do |row|
+    Video.create!(row.to_hash)
+end
