ffa0d46465ab5ef5b705854c8695f71b0676b5ff
Will Hohyon Ryu <hohyonryu@twitter.com>
2015-05-11 00:06:27 -0700

    added the user ids

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -4,7 +4,7 @@ class VideosController < ApplicationController
   # GET /videos
   # GET /videos.json
   def index
-    @videos = Video.all
+    @videos = Video.order("RANDOM()").first(100)
   end
 
   # GET /videos/1
