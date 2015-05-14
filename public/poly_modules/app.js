5b214544320e13d5706a6b54f4d75ee85d84dcbf
elegantuniv <khpark@knowre.com>
2015-05-14 15:21:11 +0900

    design updates with mockup

--- public/poly_modules/app.js
+++ public/poly_modules/app.js
@@ -1,10 +1,82 @@
 var temp_app = document.querySelector('#temp_app');
 
 temp_app.groups = [		    	
-	{title: "The Last King of Scotland", img: "https://i.ytimg.com/vi/ASO_zypdnsQ/hqdefault.jpg" },
-	{title: "Frost/Nixon", img: "https://i.ytimg.com/vi/ASO_zypdnsQ/hqdefault.jpg"},
-	{title: "Remember the Titans", img: "https://i.ytimg.com/vi/ASO_zypdnsQ/hqdefault.jpg"},
-	{title: "Saint Ralph", img: "http://img.youtube.com/vi/S7f2DISpkw0/0.jpg"},
-	{title: "Dead Man's Shoes", img: "http://img.youtube.com/vi/Xt11kOEhOMc/0.jpg"},
-	{title: "180 South", img: "http://img.youtube.com/vi/ANE5j1zUExU/0.jpg"}		
+	{
+		singer: "Girls' Generation", 
+		title: "Oh", 
+		img: "https://i.ytimg.com/vi/ASO_zypdnsQ/hqdefault.jpg", 
+		official: true,
+		upload_date: "Jan 26, 2010",
+		youtube_views: 5649976,
+		upvotes: 35456,
+		downvotes: 587,
+		approval_rating: "98.37%",
+		hotness: 2,
+		cheesiness: 0
+	},
+	{
+		singer: "Girls' Generation", 
+		title: "Oh", 
+		img: "https://i.ytimg.com/vi/ASO_zypdnsQ/hqdefault.jpg", 
+		official: true,
+		upload_date: "Jan 26, 2010",
+		youtube_views: 5649976,
+		upvotes: 35456,
+		downvotes: 587,
+		approval_rating: "98.37%",
+		hotness: 2,
+		cheesiness: 0
+	},
+	{
+		singer: "Girls' Generation", 
+		title: "Oh", 
+		img: "https://i.ytimg.com/vi/ASO_zypdnsQ/hqdefault.jpg", 
+		official: true,
+		upload_date: "Jan 26, 2010",
+		youtube_views: 5649976,
+		upvotes: 35456,
+		downvotes: 587,
+		approval_rating: "98.37%",
+		hotness: 2,
+		cheesiness: 0
+	},
+	{
+		singer: "Girls' Generation", 
+		title: "Oh", 
+		img: "https://i.ytimg.com/vi/ASO_zypdnsQ/hqdefault.jpg", 
+		official: true,
+		upload_date: "Jan 26, 2010",
+		youtube_views: 5649976,
+		upvotes: 35456,
+		downvotes: 587,
+		approval_rating: "98.37%",
+		hotness: 2,
+		cheesiness: 0
+	},
+	{
+		singer: "Girls' Generation", 
+		title: "Oh", 
+		img: "https://i.ytimg.com/vi/ASO_zypdnsQ/hqdefault.jpg", 
+		official: true,
+		upload_date: "Jan 26, 2010",
+		youtube_views: 5649976,
+		upvotes: 35456,
+		downvotes: 587,
+		approval_rating: "98.37%",
+		hotness: 2,
+		cheesiness: 0
+	},
+	{
+		singer: "Girls' Generation", 
+		title: "Oh", 
+		img: "https://i.ytimg.com/vi/ASO_zypdnsQ/hqdefault.jpg", 
+		official: true,
+		upload_date: "Jan 26, 2010",
+		youtube_views: 5649976,
+		upvotes: 35456,
+		downvotes: 587,
+		approval_rating: "98.37%",
+		hotness: 2,
+		cheesiness: 0
+	}	
 ];	
