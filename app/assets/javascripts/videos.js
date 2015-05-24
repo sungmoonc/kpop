7684a34914d3494bcd70ad32ea05c96c82acdfdf
Erin Hyejin Kim <lvhrhj@gmail.com>
2015-05-24 01:56:57 -0700

    render videos based on sorting, but playing video is not working

--- app/assets/javascripts/videos.js
+++ app/assets/javascripts/videos.js
@@ -1,29 +1,53 @@
-$(document).ready(function(){
-  $(".hook--showmodal").on('click', function(){
-    var url= $(this).attr("href")
+function call_sort_by(e, field, order) {
+  e.preventDefault();
+  var source = $('#indv_video_template').html();
+  var templatingFunction = Handlebars.compile(source);
+  var context = {};
+
+  $.ajax({
+    url: '/sortby/'+ field + '/' + order,
+    type: 'GET'
+  }).done(function (response) {
+    context.videos = response;
+    $("ul.thumbnails").html("");
+    $(".thumbnails").append(templatingFunction(context));
+  })
+}
+$(document).on('page:change', function () {
+  $(".hook--showmodal").on('click', function () {
+    var url = $(this).attr("href");
+    console.log("choki==========")
+    console.log(url)
     $("#iframe").attr('src', url);
   });
 
-  $("#basicModal").on('hidden.bs.modal', function(){
+  $("#basicModal").on('hidden.bs.modal', function () {
     $("#iframe").attr('src', '');
   });
 
   $('#year').slider();
 
-  $(".upvoteasc").on('click', function(e) {
-    e.preventDefault();
+  $(".upvoteasc").on('click', function (e) {
+    call_sort_by(e, "upvotes", "asc");
+  });
 
-    $.ajax({
-      url: '/sortby/upvotes/asc',
-      type: 'GET',
-    })
-        .done(function (response) {
-          $(".thumbnails").remove();
-          //$(".thumbnails").append()
+  $(".upvotedesc").on('click', function (e) {
+    call_sort_by(e, "upvotes", "desc");
+  });
 
-        })
-        .fail(function (response) {
-          console.log('fail')
-        })
-    });
+  $(".youtubeasc").on('click', function (e) {
+    call_sort_by(e, "youtube_views", "asc");
+  });
+
+  $(".youtubedesc").on('click', function (e) {
+    call_sort_by(e, "youtube_views", "desc");
+  });
+
+  $(".uploadasc").on('click', function (e) {
+    call_sort_by(e, "upload_date", "asc");
+  });
+
+  $(".uploaddesc").on('click', function (e) {
+    call_sort_by(e, "upload_date", "desc");
+  });
 });
