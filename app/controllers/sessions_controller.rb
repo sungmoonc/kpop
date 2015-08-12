985830c3855dc0a4d8130aff80118fc3e3b7d123
elegantuniv <korea651@gmail.com>
2015-08-13 01:38:10 +0900

    ..

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,3 @@
-
-class SessionsController < Devise::SessionsController  	
-    def create
-	  resource = warden.authenticate!(:scope => resource_name, :recall => "main#index")
-	  sign_in_without_redirect(resource_name, resource)
-	  @after_sign_in_path = after_sign_in_path_for(resource)
-	end
-
-	def sign_in_without_redirect(resource_or_scope, resource=nil)
-	  scope = Devise::Mapping.find_scope!(resource_or_scope)
-	  resource ||= resource_or_scope
-	  sign_in(scope, resource) unless warden.user(scope) == resource
-	end
+class SessionsController < Devise::SessionsController  
+    respond_to :json
 end  
